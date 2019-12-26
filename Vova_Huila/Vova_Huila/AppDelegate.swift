1cccddb9313126d6bd0b9508159d0c41b1080c5c
Aleksey <aleksey@nonamecreative.studio>
2019-12-26 15:00:34 -0600

    responded to comments

--- Vova_Huila/Vova_Huila/AppDelegate.swift
+++ Vova_Huila/Vova_Huila/AppDelegate.swift
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // sho tut proishodit, nahuy
+    // nihuia
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
