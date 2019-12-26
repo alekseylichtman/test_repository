c3d8cfaf3a3be33f20be37e8d87094342d31ed9e
Aleksey <aleksey@nonamecreative.studio>
2019-12-26 15:04:57 -0600

    dobavil eshe gonna na lopatu

--- Vova_Huila/Vova_Huila/AppDelegate.swift
+++ Vova_Huila/Vova_Huila/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // sho tut proishodit, nahuy
     // nihuia
+    // sho ebat
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
